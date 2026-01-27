1dac308d4529469601cd5eb92931cc8cb687ceee
gathanuarobert <robertgathanua@gmail.com>
2026-01-27 16:30:22 +0300

    Fill productData.js file with mock data.

---
+++ my-app/src/data/productData.js
@@ -0,0 +1,49 @@
+/**
+ * TRENDYTECH INVENTORY DATA
+ * Fill in the name, price, description, and image URL for each item.
+ */
+
+export const cctvData = [
+  {
+    id: 1,
+    name: "",
+    image: "",
+    price: "Ksh ",
+    desc: ""
+  },
+  { id: 2, name: "", image: "", price: "Ksh ", desc: "" },
+  { id: 3, name: "", image: "", price: "Ksh ", desc: "" },
+  { id: 4, name: "", image: "", price: "Ksh ", desc: "" },
+  { id: 5, name: "", image: "", price: "Ksh ", desc: "" },
+  // ... Keep adding until 31 or 40
+];
+
+export const fenceData = [
+  {
+    id: 1,
+    name: "",
+    image: "",
+    price: "Ksh ",
+    desc: ""
+  },
+  { id: 2, name: "", image: "", price: "Ksh ", desc: "" },
+  { id: 3, name: "", image: "", price: "Ksh ", desc: "" },
+  { id: 4, name: "", image: "", price: "Ksh ", desc: "" },
+  { id: 5, name: "", image: "", price: "Ksh ", desc: "" },
+  // ... Keep adding until 31 or 40
+];
+
+export const electronicsData = [
+  {
+    id: 1,
+    name: "",
+    image: "",
+    price: "Ksh ",
+    desc: ""
+  },
+  { id: 2, name: "", image: "", price: "Ksh ", desc: "" },
+  { id: 3, name: "", image: "", price: "Ksh ", desc: "" },
+  { id: 4, name: "", image: "", price: "Ksh ", desc: "" },
+  { id: 5, name: "", image: "", price: "Ksh ", desc: "" },
+  // ... Keep adding until 31 or 40
+];
