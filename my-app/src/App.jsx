f7ca1ab1f48c541836686a1647ea9211613ec78e
gathanuarobert <robertgathanua@gmail.com>
2026-01-27 17:12:36 +0300

    Solve the links.

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,12 +1,32 @@
 import React from "react";
-import { BrowserRouter, Route, Routes} from "react-router-dom";
+import { Route, Routes} from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
+import CctvInstallation from "./components/CctvInstallation";
+import ElectricFence from "./components/ElectricFence";
+import Electronics from "./components/Electronics";
+import Recovery from "./components/Recovery";
+import ElectricFenceInstallation from "./components/ElectricFenceInstallation";
+import CCTVKits from "./components/CCTVKits";
+import Contacts from "./pages/Contacts";
+import Products from "./pages/Products";
+import Services from "./pages/Services";
+import WhyUs from "./pages/WhyUs";
 
 const AppRoutes = () => {
   return (
     
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path='/contacts' element={<Contacts />} />
+        <Route path='/products' element={<Products />} />
+        <Route path='/services' element={<Services />} />
+        <Route path='/whyUs' element={<WhyUs />} />
+        <Route path='/cctvInstallation' element={<CctvInstallation />} />
+        <Route path='/electricFence' element={<ElectricFence />} />
+        <Route path='/electronics' element={<Electronics />} />
+        <Route path='/recovery' element={<Recovery />} />
+        <Route path='/electricFenceInstallation' element={<ElectricFenceInstallation />} />
+        <Route path='/cctvKits' element={<CCTVKits />} />
       </Routes>
     
   );
